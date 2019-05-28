e7781d12dc9f6b68938dca4dad5cf7a80eb22f8f
Josh <joshua.hansen188@gmail.com>
2019-05-28 21:05:35 +1000

    updated time for australia. removed unused status

--- dwmstatus.c
+++ dwmstatus.c
@@ -17,6 +17,7 @@
 
 #include <X11/Xlib.h>
 
+char *tzaustralia = "Australia/Melbourne";
 char *tzargentina = "America/Buenos_Aires";
 char *tzutc = "UTC";
 char *tzberlin = "Europe/Berlin";
@@ -181,10 +182,7 @@ main(void)
 	char *status;
 	char *avgs;
 	char *bat;
-	char *bat1;
-	char *tmar;
-	char *tmutc;
-	char *tmbln;
+	char *tmmel;
 	char *t0, *t1, *t2;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
@@ -194,18 +192,14 @@ main(void)
 
 	for (;;sleep(60)) {
 		avgs = loadavg();
-		bat = getbattery("/sys/class/power_supply/BAT0");
-		bat1 = getbattery("/sys/class/power_supply/BAT1");
-		tmar = mktimes("%H:%M", tzargentina);
-		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
+		bat = getbattery("/sys/class/power_supply/BAT1");
+		tmmel = mktimes("%a %d %b %H:%M %Z %Y", tzaustralia);
 		t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
 		t1 = gettemperature("/sys/devices/virtual/hwmon/hwmon2", "temp1_input");
 		t2 = gettemperature("/sys/devices/virtual/hwmon/hwmon4", "temp1_input");
 
-		status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s",
-				t0, t1, t2, avgs, bat, bat1, tmar, tmutc,
-				tmbln);
+		status = smprintf("T:%s|%s|%s L:%s B:%s | %s",
+				t0, t1, t2, avgs, bat, tmmel);
 		setstatus(status);
 
 		free(t0);
@@ -213,10 +207,7 @@ main(void)
 		free(t2);
 		free(avgs);
 		free(bat);
-		free(bat1);
-		free(tmar);
-		free(tmutc);
-		free(tmbln);
+		free(tmmel);
 		free(status);
 	}
 
